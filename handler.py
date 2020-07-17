83de068c063444574765ff3f5bfef145c194d423
Akshat Jaipuria <38358340+akshatjaipuria@users.noreply.github.com>
2020-07-17 22:53:24 +0530

    Add files via upload

---
+++ handler.py
@@ -0,0 +1,99 @@
+try:
+    import unzip_requirements
+except ImportError:
+    pass
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+
+import boto3
+import os
+import io
+import json
+import base64
+from requests_toolbelt.multipart import decoder
+print("Import End...")
+
+#defining environment variables
+S3_BUCKET = os.environ['S3_BUCKET'] if 'S3_BUCKET' in os.environ else 'mymodels'
+MODEL_PATH = os.environ['MODEL_PATH'] if 'MODEL_PATH' in os.environ else 'mobilenet_v2.pt'
+LABEL_FILE_PATH = os.environ['LABEL_FILE_PATH'] if 'LABEL_FILE_PATH' in os.environ else 'labels.txt'
+
+print("Downloading Model...")
+
+s3 = boto3.client('s3')
+
+try:
+    if os.path.isfile(MODEL_PATH) != True:
+        obj = s3.get_object(Bucket=S3_BUCKET, Key=MODEL_PATH)
+        print("Creating Bytestream")
+        bytestream = io.BytesIO(obj['Body'].read())
+        print("Loading Model...")
+        model = torch.jit.load(bytestream)
+        print("Model Loaded...")
+
+        #loading labels
+        data = s3.get_object(Bucket=S3_BUCKET, Key=LABEL_FILE_PATH)
+        contents = data['Body'].read()
+        labels = eval(contents)
+        print("Labels loaded...")
+
+except Exception as e:
+    print(repr(e))
+    raise(e)
+
+def transform_image(image_bytes):
+    try:
+        transformations = transforms.Compose([
+            transforms.Resize(255),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+        image = Image.open(io.BytesIO(image_bytes))
+        return transformations(image).unsqueeze(0)
+    except Exception as e:
+        print(repr(e))
+        raise(e)
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    return model(tensor).argmax().item()
+
+def classify_image(event, context):
+    try:
+        content_type_header = event['headers']['content-type']
+        print(event['body'])
+        body = base64.b64decode(event["body"])
+        print('BODY LOADED')
+
+        picture = decoder.MultipartDecoder(body, content_type_header).parts[0]
+        prediction = get_prediction(image_bytes=picture.content)
+        print(prediction)
+
+        filename = picture.headers[b'Content-Disposition'].decode().split(';')[1].split('=')[1]
+        if len(filename) < 4:
+            filename = picture.headers[b'Content-Disposition'].decode().split(';')[2].split('=')[1]
+        print(filename)
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                "Access-Control-Allow-Credentials": True
+            },
+            "body": json.dumps({'file': filename.replace('"', ''), 'predicted': prediction, 'class': labels[prediction]})
+        }
+    except Exception as e:
+        print(repr(e))
+        return{
+            "statusCode": 500,
+            "headers": {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True
+            },
+            "body": json.dumps({"error": repr(e)})
+        }
